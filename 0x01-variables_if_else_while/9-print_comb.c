4df0613494dfa8560d79a02fb7397d298bf6b438
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-09-11 20:12:25 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints a series of numbers with commas
+ * Description: this code prints all
+ * possible combinations of single-digit numbers
+ * using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '48'; c <= '57'; c++)
+	{
+		putchar(c);
+		if (c != '57')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
