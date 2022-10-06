75d0a37f4162a8a627a5fa75dc7363ff8928ec43
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-10-06 12:42:36 -0700

    task 0 & 1 corrected

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  * Return: character poinetr to a newly allocated place in memory
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k;
 	char *s;
@@ -19,20 +19,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 		i = 0;
 	else
 	{
-		for (i = 0; si[i]; i++);
+		for (i = 0; s[i]; i++)
+			;
 	}
 	if (s2 == NULL)
 		j = 0;
 	else
 	{
-		for (j = 0; s2[j]; j++);
+		for (j = 0; s2[j]; j++)
+			;
 	}
 	if (j > n)
 		j = n;
 	s = malloc(sizeof(char) * (i + j + 1));
-	if (s ==NULL)
+	if (s == NULL)
 		return (NULL);
-	for (k = o; k < j; k++)
+	for (k = 0; k < j; k++)
 		s[k + i] = s2[k];
 	s[i + j] = '\0';
 	return (s);
