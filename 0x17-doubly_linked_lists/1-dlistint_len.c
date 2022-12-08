ceedd6ec78233a7a386db44aec32b2c42c6abf61
Richard Ben-Aboje <richardcyph17@gmail.com>
2022-12-08 11:03:18 -0800

    task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function counts the number of elements 
+ * in a doubly linked dlistint_t list.
+ * @h: The head of the doubly linked listint_t list.
+ *
+ * Return: The total number of elements in the dlistint_t list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
